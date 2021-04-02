e24b5ea64d5917dcaf1e791a23a40953c0f28b6f
Nikolov0080 <apofis12343@gmail.com>
2021-04-02 21:55:15 +0300

    a

--- wear-it/src/components/products/homeMenu.js
+++ wear-it/src/components/products/homeMenu.js
@@ -1,22 +1,41 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Context from '../../context/Context';
 import style from '../../styles/homeMenu.module.css';
 
 const HomeMenu = () => {
-// continue this with useReducer hook!!!
+    // continue this with useReducer hook!!!
     const context = useContext(Context);
-    console.log(context);
 
-    const categories = ['Hats', 'Jeans', 'Shirts', 'Jackets',];
+    const [categories, setCategories] = useState([
+        { type: 'Hats', view: "inactive" },
+        { type: 'Jeans', view: "inactive" },
+        { type: 'Shirts', view: "inactive" },
+        { type: 'Jackets', view: "inactive" }
+    ]);
+
+    const changeStyle = (index) => {
+
+        categories.map((cat, i) => {
+            if (i === index) {
+                cat.view = "active";
+            } else {
+                cat.view = "inactive";
+            }
+            return categories;
+        })
+
+        console.log(categories)
+        setCategories(categories)
+
+    }
 
     return (
         <div>
             <div>
-                {categories.map((cat, index) => {
-                    return <div key={index}></div>
+                {categories.map(({ type, view }, index) => {
+                    return <div className={style[view]} onClick={() => changeStyle(index)} key={index} >{type}</div>
                 })}
             </div>
-
         </div>
     )
 }
