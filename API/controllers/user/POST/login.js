0f41aeef30516068aabb66470a50ea1e39b9e8da
Nikolov0080 <apofis12343@gmail.com>
2021-04-17 13:13:45 +0300

    fixed login issue

--- API/controllers/user/POST/login.js
+++ API/controllers/user/POST/login.js
@@ -14,6 +14,7 @@ module.exports.login = (req, res) => {
 
     } else {
         userSchema.findOne({ username }).then(resp => {
+            
             if (resp) {
                 bcrypt.compare(password, resp.password, (err, result) => {
                     if (err) {
